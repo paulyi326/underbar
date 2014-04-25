ab4bc56256441faceff9679496ace9fe32c871b8
Paul Yi <paul.yi326@gmail.com>
2014-04-24 18:29:31 -0700

    Re-implemented last

--- src/underbar.js
+++ src/underbar.js
@@ -39,10 +39,7 @@ var _ = {};
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
-  	if (n > array.length) {
-  		n = array.length;
-  	}
-  	return n === undefined ? array[array.length - 1] : array.slice(array.length - n, array.length);
+  	return n === undefined ? array[array.length - 1] : array.slice(Math.max(0, array.length - n));
   };
 
   // Call iterator(value, key, collection) for each element of collection.
